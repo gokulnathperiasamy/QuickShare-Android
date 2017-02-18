1d9bde8d6f7075b8d39acd7e32f1b4ca5ccfbe33
Gokul Nath Periasamy <gokul-nath.periasamy@hpe.com>
2017-02-18 22:04:27 +0530

    Dummy data for Card View.

--- mobile/src/main/java/com/quickshare/adapter/CardListItemViewHolder.java
+++ mobile/src/main/java/com/quickshare/adapter/CardListItemViewHolder.java
@@ -3,15 +3,39 @@ package com.quickshare.adapter;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.TextView;
 
+import com.quickshare.R;
 import com.quickshare.entity.ProfileData;
 import com.quickshare.fragment.CardFragment;
+import com.quickshare.utility.ProfileDataHelper;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class CardListItemViewHolder extends RecyclerView.ViewHolder {
+
+    @Bind(R.id.name)
+    TextView name;
+
+    @Bind(R.id.company_name)
+    TextView companyName;
+
+    @Bind(R.id.company_address)
+    TextView companyAddress;
+
+    @Bind(R.id.email_id)
+    TextView emailID;
+
+    @Bind(R.id.mobile_number)
+    TextView mobileNumber;
+
+    @Bind(R.id.office_number)
+    TextView officeNumber;
+
+    @Bind(R.id.fax)
+    TextView fax;
 
     Context context;
 
@@ -23,12 +47,57 @@ public class CardListItemViewHolder extends RecyclerView.ViewHolder {
     public void bindView(final ProfileData profileData, final Context context, final CardFragment cardFragment) {
         this.context = context;
 
+        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        int width = windowManager.getDefaultDisplay().getWidth();
+        itemView.setLayoutParams(new RecyclerView.LayoutParams(width, RecyclerView.LayoutParams.WRAP_CONTENT));
         itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Do nothing...
             }
         });
+
+        setData(profileData);
+    }
+
+    private void setData(ProfileData profileData) {
+        name.setText(ProfileDataHelper.getFullName(profileData.firstName, profileData.lastName));
+
+        if (profileData.companyName.length() > 0) {
+            companyName.setText(profileData.companyName);
+        } else {
+            companyName.setVisibility(View.GONE);
+        }
+
+        if (profileData.companyAddress.length() > 0) {
+            companyAddress.setText(profileData.companyAddress);
+        } else {
+            companyAddress.setVisibility(View.GONE);
+        }
+
+        if (profileData.email.length() > 0) {
+            emailID.setText(ProfileDataHelper.getFormattedEmail(profileData.email));
+        } else {
+            emailID.setVisibility(View.GONE);
+        }
+
+        if (profileData.mobileNumber.length() > 0) {
+            mobileNumber.setText(ProfileDataHelper.getFormattedMobileNumber(profileData.mobileNumber));
+        } else {
+            mobileNumber.setVisibility(View.GONE);
+        }
+
+        if (profileData.officeNumber.length() > 0) {
+            officeNumber.setText(ProfileDataHelper.getFormattedOfficeNumber(profileData.officeNumber));
+        } else {
+            officeNumber.setVisibility(View.GONE);
+        }
+
+        if (profileData.fax.length() > 0) {
+            fax.setText(ProfileDataHelper.getFormattedFax(profileData.fax));
+        } else {
+            fax.setVisibility(View.GONE);
+        }
     }
 
 }
--- mobile/src/main/java/com/quickshare/utility/ProfileDataHelper.java
+++ mobile/src/main/java/com/quickshare/utility/ProfileDataHelper.java
@@ -1,15 +1,72 @@
 package com.quickshare.utility;
 
 import com.quickshare.entity.ProfileData;
+import com.quickshare.entity.ProfileDataType;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 public class ProfileDataHelper {
 
     public static String getProfileID() {
-        return "_ID" + "|" + Math.random() + "|" + System.nanoTime();
+        return "_ID" + "|" + generateRandomNumber(10, 99) + "|" + System.nanoTime();
     }
 
     public static boolean isDataValid(ProfileData profileData) {
         return profileData != null && profileData.firstName.length() > 0;
+    }
+
+    public static String getFullName(String firstName, String lastName) {
+        if (lastName.length() > 0) {
+            return firstName + " " + lastName;
+        }
+        return firstName;
+    }
+
+    public static String getFormattedEmail(String email) {
+        if (email.length() > 0) {
+            return "E: " + email;
+        }
+        return "";
+    }
+
+    public static String getFormattedMobileNumber(String mobileNumber) {
+        if (mobileNumber.length() > 0) {
+            return "M: " + mobileNumber;
+        }
+        return "";
+    }
+
+    public static String getFormattedOfficeNumber(String officeNumber) {
+        if (officeNumber.length() > 0) {
+            return "O: " + officeNumber;
+        }
+        return "";
+    }
+
+    public static String getFormattedFax(String fax) {
+        if (fax.length() > 0) {
+            return "F: " + fax;
+        }
+        return "";
+    }
+
+    public static void generateDummyData() {
+        ProfileData profileData = new ProfileData();
+        profileData.profileID = ProfileDataHelper.getProfileID();
+        profileData.firstName = "First" + generateRandomNumber(0, 9);
+        profileData.lastName = "Last" + generateRandomNumber(0, 9);
+        profileData.companyName = "Company " + generateRandomNumber(10, 19);
+        profileData.companyAddress = "Company Address " + generateRandomNumber(20, 29) + " Location " + generateRandomNumber(30, 39);
+        profileData.email = "user" + generateRandomNumber(40, 41) + "@mail.com";
+        profileData.mobileNumber = "" + generateRandomNumber(100000000, 999999999);
+        profileData.officeNumber = "" + generateRandomNumber(800000000, 999999999);
+        profileData.fax = "" + generateRandomNumber(100000, 999999);
+        profileData.isMyProfile = ProfileDataType.OTHERS.getValue();
+        profileData.save();
+    }
+
+    private static int generateRandomNumber(int min, int max) {
+        return min + (int)(Math.random() * ((max - min) + 1));
     }
 
 }
